69aac174d2998837a2460b9f1716ae9a740ad9f0
Nguyen Phap <tnv218@gmail.com>
2020-11-10 12:41:31 +0700

    u

---
+++ VB/clonexp.bat
@@ -0,0 +1,2 @@
+cd "C:\Program Files\Oracle\VirtualBox\"
+VBoxManage.exe clonevm xp16 --name=xp19 --basefolder=F:\vm\xp --register
---
+++ VB/startxp13.bat
@@ -0,0 +1,2 @@
+cd "C:\Program Files\Oracle\VirtualBox\"
+VBoxManage.exe startvm "xp13" --type headless
